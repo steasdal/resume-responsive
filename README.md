46cc087e919395d7b4b26c49683fc3cb5625b3bf
Sam Teasdale <samuel.teasdale@gmail.com>
2017-06-18 13:34:32 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sam Teasdale's Resume
+A response HTML resume wrapped in a simple Node.js static web server, dockerized and deployed to the Kubernetes cluster running in my garage.
---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * Static HTTP Server
+ *
+ * Create a static file server instance to serve files
+ * and folder in the './public' folder
+ */
+
+// modules
+var static = require( 'node-static' ),
+    port = 8080,
+    http = require( 'http' );
+
+// config
+var file = new static.Server( './content', {
+    cache: 3600,
+    gzip: true
+} );
+
+// serve
+http.createServer( function ( request, response ) {
+    request.addListener( 'end', function () {
+        file.serve( request, response );
+    } ).resume();
+} ).listen( port );
+
+console.log("listening on port 8080");
